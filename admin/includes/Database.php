3baadc06e3f47ad343fd92569d5456f13d9aeb4d
tatsurodev <contacts@tatsuro.dev>
2019-03-11 23:56:41 +0900

    4-34 Databaseクラスをmysqliを使った方法に変更

--- admin/includes/Database.php
+++ admin/includes/Database.php
@@ -13,27 +13,35 @@ class Database
 
     public function open_db_connection()
     {
-        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-        if (mysqli_connect_errno()) {
-            die('Database connection failed badly.'.mysqli_error());
+        if ($this->connection->connect_errno) {
+            die('Database connection failed badly.'.$this->connection->connect_error);
         }
     }
 
     public function query($sql)
     {
-        return mysqli_query($this->connection, $sql);
+        $result = $this->connection->query($sql);
+        $this->confirm_query($result);
+
+        return $result;
     }
 
     public function escape_string($string)
     {
-        return mysqli_real_escape_string($this->connection, $string);
+        return $this->connection->real_escape_string($string);
+    }
+
+    public function the_insert_id()
+    {
+        return $this->connection->insert_id;
     }
 
     private function confirm_query($result)
     {
         if (!$result) {
-            die('Query Failed');
+            die('Query Failed'.$this->connection->error);
         }
     }
 }
