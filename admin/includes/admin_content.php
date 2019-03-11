c7d2b36a4ed252d8044bdfdff358ffa417187a3f
tatsurodev <contacts@tatsuro.dev>
2019-03-11 22:44:32 +0900

    4-33 Databaseクラスのコネクションテスト

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -7,9 +7,12 @@
                             Admin
                             <small>Subheading</small>
                         </h1>
-                        <?php if ($database->connection) {
-    echo 'true';
-}?>
+                        <?php
+                        $sql = 'SELECT * FROM users WHERE id=1';
+                        $result = $database->query($sql);
+                        $user_found = mysqli_fetch_array($result);
+                        echo $user_found['username'];
+                         ?>
                         <ol class="breadcrumb">
                             <li>
                                 <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
