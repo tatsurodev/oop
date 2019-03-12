53fa64a329915532677a638bce63f409770ab8d7
tatsurodev <contacts@tatsuro.dev>
2019-03-12 10:07:49 +0900

    5-38 Userクラスのfind_all_usersメソッドをstaticに変更

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -8,8 +8,7 @@
                             <small>Subheading</small>
                         </h1>
                         <?php
-                        $user = new User();
-                        $result_set = $user->find_all_users();
+                        $result_set = User::find_all_users();
                         while ($row = mysqli_fetch_array($result_set)) {
                             echo $row['username'].'<br>';
                         }
