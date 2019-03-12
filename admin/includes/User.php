1c614305a3983ca20ee1d03ec443f2dc44883662
tatsurodev <contacts@tatsuro.dev>
2019-03-12 15:41:27 +0900

    5-40 Userクラスに何度も使うクエリ実行用のメソッドを追加

--- admin/includes/User.php
+++ admin/includes/User.php
@@ -4,19 +4,21 @@ class User
 {
     public static function find_all_users()
     {
-        global $database;
-
-        $result_set = $database->query('SELECT * FROM users');
-
-        return $result_set;
+        return self::find_this_query('SELECT * FROM users');
     }
 
     public static function find_user_by_id($id)
     {
-        global $database;
-        $result_set = $database->query("SELECT * FROM users WHERE id={$id}");
-        $found_user = mysqli_fetch_array($result_set);
+        $result_set = self::find_this_query("SELECT * FROM users WHERE id={$id} LIMIT 1");
 
-        return $found_user;
+        return mysqli_fetch_array($result_set);
+    }
+
+    public static function find_this_query($sql)
+    {
+        global $database;
+        $result_set = $database->query($sql);
+
+        return $result_set;
     }
 }
