f92b38627bd4dc228a1be895af9ccba997a510eb
tatsurodev <contacts@tatsuro.dev>
2019-03-12 19:38:00 +0900

    5-43 Userクラスにユーザー情報をプロパティにセットするinstantationメソッドを追加

--- admin/includes/User.php
+++ admin/includes/User.php
@@ -27,4 +27,16 @@ class User
 
         return $result_set;
     }
+
+    public static function instantation($find_user)
+    {
+        $the_object = new self();
+        $the_object->id = $find_user['id'];
+        $the_object->username = $find_user['username'];
+        $the_object->password = $find_user['password'];
+        $the_object->first_name = $find_user['first_name'];
+        $the_object->last_name = $find_user['last_name'];
+
+        return $the_object;
+    }
 }
