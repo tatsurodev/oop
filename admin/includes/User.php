13e6ffb9099b132a8b6608b4d93d3d344a22f14d
tatsurodev <contacts@tatsuro.dev>
2019-03-19 02:33:52 +0900

    ユーザー名とパスワードが正しいかチェックするverify_userメソッドをUserクラスに作成

--- admin/includes/User.php
+++ admin/includes/User.php
@@ -33,6 +33,28 @@ class User
         return $the_object_array;
     }
 
+    //ログインページで使用
+    public static function verify_user($username, $password)
+    {
+        global $database;
+        $username = $database->escape_string($username);
+        $password = $database->escape_string($password);
+        $sql = "
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+                username='{$username}'
+                AND password='{$password}'
+            LIMIT
+                1
+        ";
+        $the_result_array = self::find_this_query($sql);
+
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
+    }
+
     //レコードからプロパティを自動セットするメソッド
     public static function instantation($the_record)
     {
