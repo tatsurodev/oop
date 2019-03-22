cde06832f8ab9a69e6f8bc112fea0ac1d44faffe
tatsurodev <contacts@tatsuro.dev>
2019-03-22 22:06:09 +0900

    13-105 Photoクラス独自のプロパティを作成

--- admin/includes/Photo.php
+++ admin/includes/Photo.php
@@ -8,6 +8,21 @@ class Photo extends Db_object
     public $filename;
     public $type;
     public $size;
+
+    public $tmp_path;
+    public $upload_directory = 'images';
+    public $custom_errors = [];
+    public $upload_errors_array = [
+        UPLOAD_ERR_OK => 'There is no error.',
+        UPLOAD_ERR_INI_SIZE => 'The uploaded file exceeds the upload_max_filesize directive in php.ini.',
+        UPLOAD_ERR_FORM_SIZE => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML file.',
+        UPLOAD_ERR_PARTIAL => 'The uploaded file was only partially uploaded.',
+        UPLOAD_ERR_NO_FILE => 'No file was uploaded.',
+        UPLOAD_ERR_NO_TMP_DIR => 'Missing a temporary folder.',
+        UPLOAD_ERR_CANT_WRITE => 'Failed to write file to disk.',
+        UPLOAD_ERR_EXTENSION => 'A PHP extension stopped the file upload.',
+    ];
+
     protected static $db_table = 'photos';
     protected static $db_table_fields = ['photo_id', 'title', 'description', 'filename', 'type', 'size'];
 }
