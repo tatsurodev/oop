bbff55081ac131ad589910d12d64b94c3ef7dfb8
tatsurodev <contacts@tatsuro.dev>
2019-03-22 17:40:23 +0900

    13-102 Userクラスを更に抽象化、親のDb_objectクラスにメソッドを移行

--- admin/includes/Db_object.php
+++ admin/includes/Db_object.php
@@ -53,6 +53,104 @@ class Db_object
         // ))
     }
 
+    public function save()
+    {
+        return isset($this->id) ? $this->update() : $this->create();
+    }
+
+    public function create()
+    {
+        global $database, $i;
+        $properties = $this->clean_properties();
+        $sql = "
+            INSERT INTO
+                {$i(static::$db_table)} (
+                    {$i(implode(',', array_keys($properties)))}
+                )
+            VALUES
+                (
+                    '{$i(implode("','", array_values($properties)))}'
+                )
+            ;
+        ";
+
+        if ($database->query($sql)) {
+            $this->id = $database->the_insert_id();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public function update()
+    {
+        global $database, $i;
+        $properties = $this->clean_properties();
+        $properties_pairs = [];
+        foreach ($properties as $key => $value) {
+            $properties_pairs[] = "{$key}='{$value}'";
+        }
+        $sql = "
+            UPDATE
+                {$i(static::$db_table)}
+            SET
+                {$i(implode(', ', $properties_pairs))}
+            WHERE
+                id = {$database->escape_string}({$this->id})
+            ;
+        ";
+
+        $database->query($sql);
+
+        return 1 === mysqli_affected_rows($database->connection) ? true : false;
+    }
+
+    public function delete()
+    {
+        global $database, $i;
+        $sql = "
+            DELETE FROM
+                {$i(static::$db_table)}
+            WHERE
+                id = {$database->escape_string}({$this->id})
+            LIMIT
+                1
+            ;
+        ";
+
+        $database->query($sql);
+
+        return 1 === mysqli_affected_rows($database->connection) ? true : false;
+    }
+
+    //createで使用するプロパティのキーと値を$db_table_fieldsを使って取得する
+    protected function properties()
+    {
+        $properties = [];
+        foreach (static::$db_table_fields as $db_field) {
+            if (property_exists($this, $db_field)) {
+                $properties[$db_field] = $this->{$db_field};
+            }
+        }
+
+        return $properties;
+    }
+
+    //createで使用するプロパティの値をエスケープする
+    protected function clean_properties()
+    {
+        global $database;
+
+        $clean_properties = [];
+
+        foreach ($this->properties() as $key => $value) {
+            $clean_properties[$key] = $database->escape_string($value);
+        }
+
+        return $clean_properties;
+    }
+
     //与えた引数がこのユーザークラスのプロパティにあるかどうかを返すメソッド
     private function has_the_attribute($the_attribute)
     {
--- admin/includes/init.php
+++ admin/includes/init.php
@@ -5,4 +5,5 @@ require_once 'new_config.php';
 require_once 'Database.php';
 require_once 'Db_object.php';
 require_once 'User.php';
+require_once 'Photo.php';
 require_once 'Session.php';
